8f8843800bbb4bed2ee8196c6b03c3312fb6afe4
Honcy Ye <yeholdon@gmail.com>
2023-01-11 12:08:35 +0800

    change spectre which-key

--- lua/user/conf/whichkey.lua
+++ lua/user/conf/whichkey.lua
@@ -178,7 +178,7 @@ local mappings = {
     },
   },
 
-  R = {
+  r = {
     name = "Replace",
     f = { "<cmd>lua require('spectre').open_file_search()<CR>", "Replace File" },
     p = { "<cmd>lua require('spectre').open()<CR>", "Replace Project" },
